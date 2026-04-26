3c3ad88b6217ba3afb0ff4fce8d3e483c06336f3
SurvivorO <harshitbansalboy@gmail.com>
2026-04-26 22:32:17 +0530

    added authentication using clerk

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/react'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-      <App />
+      <ClerkProvider>
+          <App />
+      </ClerkProvider>
     </BrowserRouter>,
 )
